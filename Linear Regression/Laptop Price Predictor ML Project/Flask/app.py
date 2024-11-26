0fc73e6ba8fc94139f8d06c538ddddf91e43b9e3
AyushVaZe67 <145156595+AyushVaZe67@users.noreply.github.com>
2024-11-26 15:24:27 +0530

    Add files via upload

--- Linear Regression/Laptop Price Predictor ML Project/Flask/app.py
+++ Linear Regression/Laptop Price Predictor ML Project/Flask/app.py
@@ -4,7 +4,6 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load the model and dataframe
 model = pickle.load(open('pipe.pkl', 'rb'))
 df = pickle.load(open('df.pkl', 'rb'))
 
@@ -15,7 +14,6 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Retrieve form data
         company = request.form.get('company',df['Company'].unique())
         typename = request.form.get('typename',df['TypeName'].unique())
         ram = int(request.form.get('ram'))
@@ -29,18 +27,11 @@ def predict():
         ssd = int(request.form.get('ssd'))
         gpubrand = request.form.get('gpubrand',df['Gpu brand'].unique())
         os = request.form.get('os',df['os'].unique())
-        
-        # Calculate PPI (Pixels Per Inch) from screen resolution
         X_res, Y_res = map(int, resolution.split('x'))
         ppi = ((X_res ** 2) + (Y_res ** 2)) ** 0.5 / screen_size
-
-        # Prepare input for prediction
         input_query = np.array([[company, typename, ram, weight, touchscreen, ips, ppi, cpubrand, hdd, ssd, gpubrand, os]], dtype=object)
-        
-        # Perform prediction
         predicted_price = model.predict(input_query)[0]
         
-        # Return the result as JSON
         return jsonify({'Predicted Price': str(int(np.exp(predicted_price)))})
     
     except Exception as e:
